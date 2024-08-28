d08d76117f4f0fb8183503093682943aefeec4e3
Steve Wall <steven.wall@capstonec.com>
2024-08-28 22:49:05 +0000

    Remove flux from prod

--- flux/prod/main.tf
+++
@@ -1,7 +0,0 @@
-module "flux_module" {
-    source = "../base"
-
-    k8s-cluster = "prod"
-    github-private-key = "/home/stwall/.ssh/id_ed25519"
-    kube-config-path = "/home/stwall/.kube/config"
-}
